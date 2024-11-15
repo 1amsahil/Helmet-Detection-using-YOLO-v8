8f16f59515ad0b987642044d99298704949ff0d2
Mohd  Sahil <131834950+1amsahil@users.noreply.github.com>
2024-11-15 19:31:43 +0530

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 from ultralytics.models.yolo.detect import DetectionPredictor
 import cv2
 
-model =YOLO("Helmet.pt")
+model =YOLO("AdvHelmet.pt")
 result =model.predict(source ="0", show =True, conf= 0.5)
 print(result)
